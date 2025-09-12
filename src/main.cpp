e21de39d9b0ba353069693462c4b4a1bb0083fd8
Christopher Zheng <cwzheng1905@gmail.com>
2025-09-12 01:01:03 -0500

    Set up display with proper drivers

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
+/*
+Author: Christopher Zheng
+Project: Fishing Weather Report
+Description: Connects to weatherapi service to retreive weather information
+in order to calculate fishing quality for the day to load onto a TFT display
+*/
+
 #include <Arduino.h>
 #include <HTTPClient.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+void tftInit() {
+  tft.init();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(10,10);
+  tft.println("TEST");
+}
 
 void setup() {
-  
+  tftInit();
 }
 
 void loop() {
